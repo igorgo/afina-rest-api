11a53686bf86f93f67ca6c213229985ad239f481
igorgo <igorgo16@gmail.com>
2017-07-14 07:09:00 +0300

    v.1

--- README.md
+++ README.md
@@ -1,22 +1,53 @@
 # Rest api for Afina SQL
 
 example
-
-`const Server = require('afina-rest-api')
+```javascript
+const 
+    Server = require('afina-rest-api'),
+    path = require('path')
 
 let server = new Server({
     // server parameters
     "port": 3000,
+    "log": path.join(__dirname, 'log'),
+    "static": path.join(__dirname, 'dist'),
     "db": require('oracledb'),
     // anonymous user
-    "user": '*****',
-    "password": '*****',
-    "connectString": "*****",
-    "schema": "******",
+    "user": 'PARUSWEB',
+    "password": "parusweb",
+    "connectString": "P852ONE",
+    "schema": "VMF",
+    "release": 8,
     // session params
     "application": 'Admin',
     "company": 'Организация',
-    "language": 'RUSSIAN'
+    "language": 'RUSSIAN',
+
 })
 
-server.start()`
+server.apiRouter.get('/agents', async (req, res, next) => {
+    const inParams = req.query;
+    let sql = 'select RN, AGNABBR, AGNNAME, AGNIDNUMB from V_AGNLIST'
+    let binds = {}
+    if (inParams.type) {
+        sql += ' where AGNTYPE = :AGNTYPE'
+        binds["AGNTYPE"] = inParams.type
+    }
+    try {
+        const conn = await server.getConnection(req)
+        try {
+
+            rows = (await conn.execute(sql, binds)).rows
+            res.status(200).json(rows)
+        }
+        finally {
+            conn.close()
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+server.start()
+```
--- index.js
+++ index.js
@@ -6,99 +6,75 @@ const
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    session = require('express-session'),
-    SQLiteStore = require('connect-sqlite3')(session),
-    http = require('http')
-
-const normalizePort = (val) => {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
-}
-
-process.stdin.resume();//so the program will not close instantly
-
-const exitHandler = (options, err) => {
-    if (options.cleanup) console.log('clean')
-    if (err) console.log(err.stack)
-    if (options.exit) {
-        console.log('server stopped')
-        process.exit()
-    }
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null, {cleanup: true}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit: true}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit: true}))
+    fs = require('fs'),
+    rfs = require('rotating-file-stream'),
+    http = require('http'),
+    tokenHeader = 'x-afs-token'
 
 /**
- * creates Afina SQL api server
+ * Creates Afina SQL api server
  * @param {Object} config
  * @param {number} config.port Server's port.
- * @param {Object} config.db oracledb object.
- * @param {Object} config.user The database user name.
- * @param {Object} config.password The password of the database user.
- * @param {Object} config.connectString The Oracle database instance to connect to. </br>The string can be an Easy Connect string, or a Net Service Name from a tnsnames.ora file, or the name of a local Oracle database instance.
- * @param {Object} config.schema The Afina SQL schema.
- * @param {Object} config.application The Afina SQL application code.
- * @param {Object} config.company The Afina SQL company.
- * @param {Object} config.language The Afina SQL language.
+ * @param {string} config.log directory to log requests
+ * @param {Object<oracledb.Oracledb>} config.db oracledb object.
+ * @param {string} config.user The database user name.
+ * @param {string} config.password The password of the database user.
+ * @param {string} config.connectString The Oracle database instance to connect to. </br>The string can be an Easy Connect string, or a Net Service Name from a tnsnames.ora file, or the name of a local Oracle database instance.
+ * @param {string} config.schema The AfinaSQL schema.
+ * @param {number} config.release The AfinaSQL release number.
+ * @param {string} config.application The Afina SQL application code.
+ * @param {string} config.company The AfinaSQL company.
+ * @param {string} config.language The AfinaSQL language.
  *
  */
 class AfinaApiServer {
     constructor(config) {
         this.config = config
         this.express = express()
-        this.express.use(logger('dev'))
         this.express.use(bodyParser.json())
         this.express.use(bodyParser.urlencoded({extended: false}))
         this.express.use(cookieParser())
         // allow cross origin requests
         this.express.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header('Access-Control-Allow-Headers', 'Content-Type');
-            next();
+            res.header('Access-Control-Allow-Origin', '*')
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+            res.header('Access-Control-Allow-Headers', 'Content-Type')
+            next()
         })
-        this.express.use(session({
-            store: new SQLiteStore,
-            secret: 'papuaz',
-            resave: false,
-            saveUninitialized: false,
-            cookie: {maxAge: 2 * 60 * 60 * 1000} // 2 hour
-        }))
-        this.apiRouter = express.Router()
-        this.pubRouter = express.Router()
-        this.express.use('/api', this.apiRouter)
-        this.express.use('/pub', this.pubRouter)
+        // set logger
+        if (this.config.hasOwnProperty("log") && this.config.log) {
+            fs.existsSync(this.config.log) || fs.mkdirSync(this.config.log)
+            // create a rotating write stream
+            const accessLogStream = rfs('access.log', {
+                interval: '1d', // rotate daily
+                path: this.config.log
+            })
+            this.express.use(logger(
+                `:req[${tokenHeader}] :res[${tokenHeader}] :date :method :url :status :res[content-length] - :response-time ms`,
+                {stream: accessLogStream}
+            ))
+        }
+        this._apiRouter = express.Router()
+        this.express.use('/api', this._apiRouter)
+        // set static content
+        if (this.config.hasOwnProperty("static") && this.config.static) {
+            this.express.use(express.static(this.config.static))
+            this.express.use('/', (req, res) => {res.redirect('index.html')})
+        }
     }
 
+    /**
+     * Starts the server
+     * @returns {Promise.<void>}
+     */
     async start() {
-        this.config.port = normalizePort(this.config.port || '3000')
+        this.config.port = AfinaApiServer.normalizePort(this.config.port || '3000')
         this.express.set('port', this.config.port)
-        this.pubRouter.post('/login',this._login.bind(this))
-        this.pubRouter.post('/logoff',this._logoff.bind(this))
-        /* this.express.use((req, res, next) => {
-            let err = new Error('Not Found');
-            err.status = 404;
-            next(err);
-        }); */
-        this.express.use(function (err, req, res, next) {
+        this._apiRouter.post('/login', this._login.bind(this))
+        this._apiRouter.post('/logoff', this._logoff.bind(this))
+        this.express.use(function (err, req, res) {
             console.log(err.message);
             if (err.message.startsWith('ORA-20103: Дальнейшая работа в Системе невозможна')) {
-                req.session.destroy();
                 res.sendStatus(401);
             } else {
                 res.locals.message = err.message;
@@ -107,9 +83,11 @@ class AfinaApiServer {
                 res.send(err.message);
             }
         });
+
         this.db = this.config.db
         this.db.outFormat = this.db.OBJECT
         this.pool = await this._createPool()
+
         this.server = http.createServer(this.express)
         this.server.listen(this.config.port)
         this.server.on('error', error => {
@@ -136,22 +114,62 @@ class AfinaApiServer {
             const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
             console.log('Listening on ' + bind);
         })
+
+        process.stdin.resume();//so the program will not close instantly
+        //catches closing app
+        process.on('exit', this._exitHandler.bind(this, {cleanup: true}));
+        //catches ctrl+c event
+        process.on('SIGINT', this._exitHandler.bind(this, {exit: true, cleanup: true}));
+        //catches uncaught exceptions
+        process.on('uncaughtException', this._exitHandler.bind(this, {exit: true, cleanup: true}))
     }
 
-    async getConnection(token) {
+    /**
+     * Exit routine
+     * @param options
+     * @param err
+     * @returns {Promise.<void>}
+     * @private
+     */
+    async _exitHandler(options, err) {
+        if (options.cleanup) {
+            await this.pool.close()
+            console.log('Oracle connection pool terminated')
+        }
+        if (err) console.log(err.stack)
+        if (options.exit) {
+            console.log('Server stopped')
+            process.exit()
+        }
+    }
+
+    /**
+     * Returns an oracle connection from the pool,
+     * sets the schema and switches the utilizer context
+     * @param req
+     * @returns {Promise.<oracledb.Connection>}
+     */
+    async getConnection(req) {
+        if (!req.headers.hasOwnProperty(tokenHeader)) {
+            throw new Error('Unauthorized', 401)
+        }
+        const token = req.headers[tokenHeader]
         const conn = await this.pool.getConnection()
         await conn.execute(`alter session set CURRENT_SCHEMA = ${this.config.schema}`)
-        if (token) {
-            await conn.execute(
-                `begin
+        await conn.execute(
+            `begin
               PKG_SESSION.VALIDATE_WEB(SCONNECT => :SCONNECT);
             end;`, [token])
-        }
         return conn
     }
 
+    /**
+     * Creates an oracle connection pool
+     * @returns {Promise.<oracledb.Pool>}
+     * @private
+     */
     async _createPool() {
-        console.log('pool is created')
+        console.log('oracle connection pool created')
         return await this.db.createPool({
             user: this.config.user,
             password: this.config.password,
@@ -159,43 +177,72 @@ class AfinaApiServer {
         })
     }
 
+    /**
+     * Generates the 48-byte token
+     * @returns {Promise.<string>}
+     */
     static async getNewToken() {
-        let t = await require('crypto').randomBytes(48)
+        let t = await require('crypto').randomBytes(24)
         return t.toString('hex')
     }
 
+    /**
+     * Port/pipe normalizer
+     * @param val
+     * @returns {int|string}
+     */
+    static normalizePort(val) {
+        const port = parseInt(val, 10);
+        if (isNaN(port)) {
+            // named pipe
+            return val;
+        }
+        if (port >= 0) {
+            // port number
+            return port;
+        }
+        return false;
+    }
+
+    /** End user login.
+     * @param req
+     * @param res
+     * @param next
+     * @returns {Promise.<void>}
+     * @private
+     */
     async _login(req, res, next) {
         const inParams = req.body;
-        const conn = await this.getConnection()
-        const token =  await AfinaApiServer.getNewToken()
-        try {
-            await conn.execute(
-                `begin
+        const conn = await this.pool.getConnection()
+        const token = await AfinaApiServer.getNewToken()
+        await conn.execute(`alter session set CURRENT_SCHEMA = ${this.config.schema}`)
+        const sql = `begin
                    PKG_SESSION.LOGON_WEB(SCONNECT        => :SCONNECT,
                                          SUTILIZER       => :SUTILIZER,
                                          SPASSWORD       => :SPASSWORD,
                                          SIMPLEMENTATION => :SAPPLICATION,
                                          SAPPLICATION    => :SAPPLICATION,
                                          SCOMPANY        => :SCOMPANY,
-                                         SBROWSER        => :SBROWSER,
+                                         ${this.config.release >= 8 ? 'SBROWSER        => :SBROWSER' : ''},
                                          SLANGUAGE       => :SLANGUAGE);
-                 end;`,
-                {
-                    "SCONNECT": token,
-                    "SUTILIZER": inParams.username,
-                    "SPASSWORD": inParams.password,
-                    "SAPPLICATION": this.config.application,
-                    "SCOMPANY": this.config.company,
-                    "SBROWSER": req.header('user-agent'),
-                    "SLANGUAGE": this.config.language
-                }
-            )
+                 end;`
+        let binds = {
+            "SCONNECT": token,
+            "SUTILIZER": inParams.username,
+            "SPASSWORD": inParams.password,
+            "SAPPLICATION": this.config.application,
+            "SCOMPANY": this.config.company,
+            "SLANGUAGE": this.config.language
+        }
+        if (this.config.release >= 8) binds["SBROWSER"] = req.header('user-agent')
+        try {
+            await conn.execute(sql, binds)
             const ncompany = (await conn.execute(
                 `select PKG_SESSION.GET_COMPANY as NCOMPANY from dual`
             )).rows[0]["NCOMPANY"]
+            res.header(tokenHeader, token)
             res.status(200).json({
-                "token": token,
-                "ncompany" : ncompany
+                "ncompany": ncompany
             })
         }
         catch (err) {
@@ -206,19 +253,32 @@ class AfinaApiServer {
         }
     }
 
+    /**
+     * End user logoff
+     * @param req
+     * @param res
+     * @param next
+     * @returns {Promise.<void>}
+     * @private
+     */
     async _logoff(req, res, next) {
-        const inParams = req.body;
-        const conn = await this.getConnection()
         try {
-            await conn.execute(' begin PKG_SESSION.LOGOFF_WEB(SCONNECT => :SCONNECT); end;',[inParams.token])
-            res.sendStatus(200)
+            const conn = await this.getConnection(req)
+            try {
+                await conn.execute(' begin PKG_SESSION.LOGOFF_WEB(SCONNECT => :SCONNECT); end;', [req.headers[tokenHeader]])
+                res.sendStatus(200)
+            }
+            finally {
+                conn.close()
+            }
         }
         catch (err) {
             next(err)
         }
-        finally {
-            conn.close()
-        }
+    }
+
+    get apiRouter() {
+        return this._apiRouter
     }
 }
 
